3902219cdfc475d4d1ec5bf86ec154033fbe65cd
Ashik Rahman <ashrhmn99@gmail.com>
2021-08-17 10:49:09 +0600

    Add endpoints

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const app = require('express')();
 
 
+app.get('/api/random/:max', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({
+        number: Math.floor(Math.random() * req.params.max)
+    }));
+})
 
+app.get('/api/random', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify({ number: Math.random() }));
+})
+
+app.post('/api/word/:arg', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    const word = req.params.arg;
+    const count = word.toLowerCase().match(/[aeiou]/gi).length
+    res.end(JSON.stringify({ vowel_count: count }));
+})
 
 
 
